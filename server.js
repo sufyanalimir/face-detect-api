78f3ce5f08b831f0cbfd25d03956e252991697bf
sufyanali <sufyanalimir313@gmail.com>
2022-06-03 11:25:57 +0530

    initial commit for face app api

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const { handleImage, handleApiCall } = image;
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "postgres",
+    password: "1234",
+    database: "faceDetect",
+  },
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("success");
+});
+
+app.post("/signin", (req, res) => {
+  signin.handleSignIn(req, res, db, bcrypt);
+});
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  handleApiCall(req, res);
+});
+
+app.listen(3030, () => {
+  console.log("app is running on port 3030");
+});
+
+/* Planning pace
+
+/               --> res = this iisss working
+/signin         --> POST = success/fail
+/register       --> POST = user
+/profile/userid --> GET = user
+/image --> PUT  --> user
+
+*/
